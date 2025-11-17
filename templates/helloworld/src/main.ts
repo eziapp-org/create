6de0478fc47881d46a8cb954c1428637451d30cf
heyManNice <3031978990@qq.com>
2025-11-17 18:31:47 +0800

    style

--- templates/helloworld/src/main.ts
+++ templates/helloworld/src/main.ts
@@ -4,11 +4,14 @@ terminal.log('Hello from TypeScript!');
 
 const input = document.querySelector('input');
 document.querySelector('button')?.addEventListener("click", () => {
-    const inputValue = input?.value || "";
-    if (inputValue.trim() != "") {
-        terminal.log(`User Input: ${inputValue}`);
-        alert(`You entered: ${inputValue}`);
-    } else {
-        terminal.log("No input provided.");
+    if (!input) {
+        return terminal.error("Input element not found.");
+    };
+    const value = input.value.trim() || "";
+    if (value === "") {
+        return terminal.log("No input provided.");
     }
+    terminal.log(`User Input: ${value}`);
+    alert(`You entered: ${value}`);
+    input.value = "";
 });
